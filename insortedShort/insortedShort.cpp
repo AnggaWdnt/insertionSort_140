db15872b9997c63f40176b472152882f3ed3f082
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2023-03-23 15:00:19 +0700

    penambahan void input

--- insortedShort/insortedShort.cpp
+++ insortedShort/insortedShort.cpp
@@ -4,7 +4,29 @@ using namespace std;
 int arr[20];
 int n;
 
+void input() {
+    while (true) {
+        cout << "Masukkan Jumlah Data Pada Array : ";
+        cin >> n;
 
+        if (n <= 20) {
+            break;
+        }
+        else {
+            cout << "\nArray yang anda masukan maksimal 20 elemnet. \n";
+        }
+    }
+    cout << endl;
+    cout << "=========================" << endl;
+    cout << "MASUKKAN ELEMENT ARRAY" << endl;
+    cout << "=========================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+
+    }
+}
 
 
 
